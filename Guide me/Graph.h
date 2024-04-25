581d0c35ccdb5e8af5f96f9e19e13feb6e8ba950
ehabmosalah <130048063+ehabmosalah@users.noreply.github.com>
2024-04-25 02:02:46 +0200

    Update Graph.h file

--- Guide me/Graph.h
+++ Guide me/Graph.h
@@ -1,8 +1,11 @@
 #pragma once
 #pragma once
 #include <unordered_map>
+#include <map>
 #include <set>
 #include <string>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -23,16 +26,18 @@ private:
     //  map string name as a key and a set of edges (
     //  each edge contain a pair of string (which is the other node name) and transportation method
     //  transportation method contains name and cost
-    unordered_map<string, set<pair<string, TravelMethod>>> map;
+    unordered_map<string, set<pair<string, TravelMethod>>> graph;
+    unordered_map<string, pair<string, TravelMethod>> path;
+    map <string, bool> visited;
 public:
     void addPath(string country1, string country2, string methodName, int cost);
-    void updatePath(string country1, string country2, string methodName, string newName, int newCost);
+    void updatePath(string country1, string country2, string methodName, string newmethodName, int newCost);
     void deletePath(string country1, string country2, string methodName);
     void addCountry(string country);
     void deleteCountry(string country);
     bool isComplete();
-    //BFS();
-    //DFS();
+    unordered_map<string, vector<pair<string, TravelMethod>>> BFS(map<string, vector<pair<string, TravelMethod>>>& adjList, string  startNode, map <string, bool> visited);
+    unordered_map<string, vector<pair<string, TravelMethod>>> DFS(map<string, vector<pair<string, TravelMethod>>>& adjList, string  startNode, map <string, bool> visited);
     //getPaths(string country1, string country2, long cost);
 };
 
