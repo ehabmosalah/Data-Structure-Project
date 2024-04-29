bdecc663fe226b64754283783ca564ce9ef24c46
ehabmosalah <130048063+ehabmosalah@users.noreply.github.com>
2024-04-29 03:23:38 +0300

    test code

--- Guide me/Source.cpp
+++ Guide me/Source.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "Graph.h"
 using namespace std;
 #define FREE_PALESTINE            \
     ios_base::sync_with_stdio(0); \
@@ -9,5 +10,14 @@ using namespace std;
 
 int main()
 {
-   
+    Graph GuideMe;
+    GuideMe.addCountry("aaa");
+    GuideMe.addCountry("bbb");
+    GuideMe.addCountry("ccc");
+    GuideMe.addPath("aaa", "bbb", "bus", 100);
+    GuideMe.addPath("aaa", "ccc", "bus", 100);
+    GuideMe.addPath("ccc", "bbb", "bus", 100);
+    cout << GuideMe.isComplete();
+
+
 }
